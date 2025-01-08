33689ceed5d5ec697d8b7cde41a557481952dd3b
premma <premma1300@gmail.com>
2025-01-08 15:39:21 +0700

    init

---
+++ src/main/java/com/example/java_practice_project/JavaPracticeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.java_practice_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPracticeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaPracticeProjectApplication.class, args);
+	}
+
+}
