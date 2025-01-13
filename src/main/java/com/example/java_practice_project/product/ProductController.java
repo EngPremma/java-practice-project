df5b2dae3145ae115e5ceb38e715960a90165c57
premma <premma1300@gmail.com>
2025-01-13 22:54:50 +0700

    get product api

--- src/main/java/com/example/java_practice_project/product/ProductController.java
+++ src/main/java/com/example/java_practice_project/product/ProductController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/products")
 public class ProductController {
@@ -22,12 +24,12 @@ public class ProductController {
     }
 
     @GetMapping("/")
-    public ResponseEntity<String> getProducts() {
+    public ResponseEntity<List<ProductDTO>> getProducts() {
         return getProductsService.execute(null);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<ProductDTO> getProductById(@PathVariable Integer id){
+    public ResponseEntity<ProductDTO> getProductById(@PathVariable Integer id) {
         return getProductService.execute(id);
     }
 }
