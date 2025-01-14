996df7ad523b987766fb2cb533da886996122a7a
premma <premma1300@gmail.com>
2025-01-14 16:17:39 +0700

    search product api

--- src/main/java/com/example/java_practice_project/product/ProductRepository.java
+++ src/main/java/com/example/java_practice_project/product/ProductRepository.java
@@ -1,9 +1,15 @@
 package com.example.java_practice_project.product;
 
 import com.example.java_practice_project.product.model.Product;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+    List<Product> findByNameContainingOrDescriptionContaining(String name, String description, Sort sort);
+
+//    @Query("S")
 }
